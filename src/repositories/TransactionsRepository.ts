5f6b8c52f304f1392f47a0f0938bb781c7566eae
BlaackRabbit <33602013+Potato@users.noreply.github.com>
2020-04-16 20:58:36 -0300

    feat: handle transaction's repository

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,19 @@
 import Transaction from '../models/Transaction';
 
+import groupBy from '../utils/array/groupBy';
+
+import isObjectEmpty from '../utils/object/isEmpty';
+
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +24,54 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    // Group Transactions by type (income | outcome)
+    const transactionsGroupByType = groupBy(this.transactions, 'type');
+
+    let income = 0;
+    let outcome = 0;
+    let total = 0;
+
+    // Check if object is empty
+    if (isObjectEmpty(transactionsGroupByType)) {
+      return { income: 0, outcome: 0, total: 0 };
+    }
+
+    // Check if income was grouped
+    if (transactionsGroupByType.income) {
+      // Use reducer to sum accumulated values of income
+      income = transactionsGroupByType.income.reduce(
+        (accumulator, currentValue) => {
+          return accumulator + currentValue.value;
+        },
+        0,
+      );
+    }
+
+    // Check if outcome was grouped
+    if (transactionsGroupByType.outcome) {
+      // Use reducer to sum accumulated values of outcome
+      outcome = transactionsGroupByType.outcome.reduce(
+        (accumulator, currentValue) => {
+          return accumulator + currentValue.value;
+        },
+        0,
+      );
+    }
+
+    // Calculate the total value
+    total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
