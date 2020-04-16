967b61b93e9dd00d2d69019bff58b789680b3bf7
BlaackRabbit <33602013+Potato@users.noreply.github.com>
2020-04-16 20:58:50 -0300

    feat: handle transaction's routes

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,22 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    // Get Transactions
+    const transactions = transactionsRepository.all();
+
+    // Get Balance
+    const balance = transactionsRepository.getBalance();
+
+    return response.json({ transactions, balance });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,7 +24,19 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    const { title, value, type } = request.body;
+
+    const createTransactionService = new CreateTransactionService(
+      transactionsRepository,
+    );
+
+    const transaction = createTransactionService.execute({
+      title,
+      type,
+      value,
+    });
+
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
